51996e1359e3a441b1cef44c612bf8d04dc359b8
Andrew Mugford <74738144+amugfordmugford@users.noreply.github.com>
2025-12-02 11:08:12 -0500

    Initial Commit

---
+++ MailAppToolBarRead/MailAppToolBarReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailAppToolBarReadApp.swift
+//  MailAppToolBarRead
+//
+//  Created by Andrew Mugford on 2025-12-02.
+//
+
+import SwiftUI
+
+@main
+struct MailAppToolBarReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
