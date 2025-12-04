0edb0e957510c6d97968415e3849d92cbf0a97a0
Andrew Mugford <74738144+amugfordmugford@users.noreply.github.com>
2025-12-04 11:36:32 -0500

    Initial Commit

---
+++ MailGlance2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MailGlance2
+//
+//  Created by Andrew Mugford on 2025-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MailGlance2/MailGlance2App.swift
@@ -0,0 +1,17 @@
+//
+//  MailGlance2App.swift
+//  MailGlance2
+//
+//  Created by Andrew Mugford on 2025-12-04.
+//
+
+import SwiftUI
+
+@main
+struct MailGlance2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
